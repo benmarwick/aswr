3224006299acd6fc2ce8aab177cd55a755bde18c
Ben Marwick <benmarwick@gmail.com>
2017-01-15 22:18:02 +1100

    add pull

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -16,3 +16,26 @@
 hello <- function() {
   print("Hello, world!")
 }
+
+
+
+#' pull a vector or scalar (vector of length one) from a data frame
+#'
+#' Sometimes we want to get a single value from a dplyr pipeline so we can use it with inline R code in R markdown. This function is an efficient method of extracting a column from a tibble as a vector. From http://stackoverflow.com/a/24730843/1036500
+#'
+#' @param x a data frame
+#' @param y a column name
+#'
+#' @return a vector or scalar
+#' @export
+#'
+#' @examples
+#'
+#' pull(iris, Species)
+#'
+pull <- function(x,y) {
+  x[,if(is.name(substitute(y))) deparse(substitute(y)) else y, drop = FALSE][[1]]
+}
+
+
+
